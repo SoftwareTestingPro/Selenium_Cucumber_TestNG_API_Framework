6712fbfb995456cdaed357bba42b3b3c2191aa92
Sushil Kumar Singh <35335691+sushilsingh76881@users.noreply.github.com>
2024-07-10 21:51:43 +0530

    Update FileHandling.java

--- src/test/java/com/qa/JavaProgramming/FileHandling.java
+++ src/test/java/com/qa/JavaProgramming/FileHandling.java
@@ -17,50 +17,65 @@ import javax.xml.transform.OutputKeys;
 import java.io.*;
 
 public class FileHandling {
+	
+	public String filePath = System.getProperty("user.dir") + "/target/employee.xml";
+	public String ModifiedFilePath = System.getProperty("user.dir") + "/target/employee-updated.xml";
 
 	@Test
 	public void CreateXML() throws Exception {
-		// Create a DocumentBuilder
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-
 		// Create a new Document
-		Document document = builder.newDocument();
+        DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+        Document doc = documentFactory.newDocumentBuilder().newDocument();
 
-		// Create root element
-		Element root = document.createElement("library");
-		document.appendChild(root);
+        // Root element
+        Element root = doc.createElement("employees");
+        doc.appendChild(root);
 
-		// Create book elements and add text content
-		Element book1 = document.createElement("Program1");
-		book1.appendChild(document.createTextNode("Java Programming"));
-		Element book2 = document.createElement("Program2");
-		book2.appendChild(document.createTextNode("Python Programming"));
-		Element book3 = document.createElement("Program3");
-		book3.appendChild(document.createTextNode("JavaScript"));
-		Element book4 = document.createElement("Program4");
-		book4.appendChild(document.createTextNode("C Programming"));
-		root.appendChild(book1);
-		root.appendChild(book2);
-		root.appendChild(book3);
-		root.appendChild(book4);
+        // Employee 1
+        Element employee1 = doc.createElement("employee");
+        employee1.setAttribute("id", "1");
 
-		// Write to XML file
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource source = new DOMSource(document);
+        Element name1 = doc.createElement("name");
+        name1.appendChild(doc.createTextNode("John Doe"));
+        employee1.appendChild(name1);
 
-		// Specify your local file path
-		StreamResult result = new StreamResult(System.getProperty("user.dir") + "/target/output.xml");
-		transformer.transform(source, result);
-		System.out.println("XML file created successfully at "+System.getProperty("user.dir") + "/target/output.xml");
+        Element position1 = doc.createElement("position");
+        position1.appendChild(doc.createTextNode("Developer"));
+        employee1.appendChild(position1);
+
+        root.appendChild(employee1);
+
+        // Employee 2
+        Element employee2 = doc.createElement("employee");
+        employee2.setAttribute("id", "2");
+
+        Element name2 = doc.createElement("name");
+        name2.appendChild(doc.createTextNode("Jane Smith"));
+        employee2.appendChild(name2);
+
+        Element position2 = doc.createElement("position");
+        position2.appendChild(doc.createTextNode("Designer"));
+        employee2.appendChild(position2);
+
+        root.appendChild(employee2);
+
+        // Create the XML file
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        DOMSource domSource = new DOMSource(doc);
+        StreamResult streamResult = new StreamResult(new File(filePath));
+
+        transformer.transform(domSource, streamResult);
+		System.out.println("XML file created successfully at "+filePath);
 	}
 
 	@Test
 	public void ReadXML() throws Exception {
 		CreateXML();
 		// Specify the file path as a File object
-		File xmlFile = new File(System.getProperty("user.dir") + "/target/output.xml");
+		File xmlFile = new File(filePath);
 		System.out.println("Reading XML file at location : "+xmlFile);
 
 		// Create a DocumentBuilder
@@ -71,7 +86,7 @@ public class FileHandling {
 		Document document = builder.parse(xmlFile);
 
 		// Access elements by tag name
-		NodeList nodeList = document.getElementsByTagName("library");
+		NodeList nodeList = document.getElementsByTagName("employee");
 		for (int i = 0; i < nodeList.getLength(); i++) {
 			Node node = nodeList.item(i);
 			System.out.println("Element Content: " + node.getTextContent());
@@ -82,7 +97,7 @@ public class FileHandling {
 	public void ModifyXML() throws Exception {
 		CreateXML();
 		// Load the XML file
-		File inputFile = new File(System.getProperty("user.dir") + "/target/output.xml");
+		File inputFile = new File(filePath);
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(inputFile);
@@ -106,9 +121,9 @@ public class FileHandling {
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer = transformerFactory.newTransformer();
 		DOMSource source = new DOMSource(doc);
-		StreamResult result = new StreamResult(new File(System.getProperty("user.dir") + "/target/ModifiedOutput.xml"));
+		StreamResult result = new StreamResult(new File(ModifiedFilePath));
 		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 		transformer.transform(source, result);
-		System.out.println("XML file updated successfully");
+		System.out.println("XML file modified successfully. Access modified file at "+ModifiedFilePath);
 	}
 }
